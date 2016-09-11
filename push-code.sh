cb954f80cc8e18b05b46fda07f0fd3867c0c3997
Avkash Chauhan <avkash@gmail.com>
2016-09-11 15:48:35 -0700

    update

--- push-code.sh
+++ push-code.sh
@@ -2,7 +2,7 @@ cp -r web/css docs/
 cp -r web/img docs/
 mkdir docs/js
 mkdir docs/js/vendor
-cp -r web/js/vendor docs/js/vendor/
+cp -r web/js/vendor docs/js/
 cp -r web/js/app.js docs/js/
 cp -r web/js/app.models.js docs/js/
 cp -r web/js/app.domain.js docs/js/
