036f65bc258018d29f78ecaeace842a004306aa9
Avkash Chauhan <avkash@gmail.com>
2016-10-15 19:41:34 -0700

    update

--- web/js/app.models.js
+++ web/js/app.models.js
@@ -391,6 +391,11 @@ function HomeViewModel() {
 
 function KeywordsViewModel(){
 
+    /*
+    Look for later
+     http://projects.delimited.io/experiments/force-bubbles/gates.html
+     http://neuralengr.com/asifr/journals/
+     */
     var self = this;
     var base = {};
     this.root = AppViewModel;
@@ -398,8 +403,10 @@ function KeywordsViewModel(){
     this.pageHeader = ko.observable("Machine Learning Keywords");
     self.pageMainHeader = ko.observable("Page Heading...");
     self.pageSubHeader = ko.observable("Sub Heading..");
-    self.pageContent = ko.observable("Content");
-
+    self.sideHeader = ko.observable("Keywords List");
+    self.pageContent = ko.observable("Content is here");
+    self.keywordsFile = ko.observable();
+    self.keywordsList = ko.observable();
 
     self.getKeywordsJsonData = function (localObjStr) {
         console.log("root.keywordJson() -> " + localObjStr);
@@ -411,16 +418,20 @@ function KeywordsViewModel(){
             if (localObj.subTitle != null) {
                 self.pageSubHeader(localObj.subTitle);
             }
-            if (localObj.contents > 0) {
-
+            if (localObj.sideTitle != null) {
+                self.sideHeader(localObj.sideTitle);
             }
-            if (localObj.links > 0) {
-
+            if (localObj.keywordsFile != null) {
+                self.keywordsFile(localObj.keywordsFile);
+                loadKeywordsFile(self.keywordsFile());
             }
         }
     };
 
+    self.loadKeywordsFile = function(fileName){
 
+        self.pageContent(fileName);
+    };
 
     self.load = function(){
         root.getKeywordsJsonFunction(root.masterKeywordId());
